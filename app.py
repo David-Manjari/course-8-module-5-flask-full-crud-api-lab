62ab0d407fb257bfeb14df220ed332c1be35f353
Manjari <manjari@Manjari.localdomain>
2026-08-14 07:07:18 +0300

    snippet of create event to post

--- app.py
+++ app.py
@@ -19,13 +19,22 @@ events = [
 
 # TODO: Task 1 - Define the Problem
 # Create a new event from JSON input
+app.route("/events", methods = ["GET"])
+def get_events()
+    pass
 @app.route("/events", methods=["POST"])
 def create_event():
     # TODO: Task 2 - Design and Develop the Code
-
+    id = 0
+    for event in event:
+        id +=1
+    data = request.get_json()
+    new_event = Event(id+1, "Python Lab Review")
     # TODO: Task 3 - Implement the Loop and Process Each Element
+events.append(new_event)  
+  # TODO: Task 4 - Return and Handle Results
+  return jsonify(new_event.to_dict()),201
 
-    # TODO: Task 4 - Return and Handle Results
     pass
 
 # TODO: Task 1 - Define the Problem
