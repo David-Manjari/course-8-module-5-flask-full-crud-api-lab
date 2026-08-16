9926b4c92cec6536c41131c43c1f56582e3f8601
Manjari <manjari@Manjari.localdomain>
2026-08-16 16:13:45 +0300

    Submission

--- app.py
+++ app.py
@@ -20,44 +20,55 @@ events = [
 # TODO: Task 1 - Define the Problem
 # Create a new event from JSON input
 app.route("/events", methods = ["GET"])
-def get_events()
-    pass
+def get_events():
+    event_list = []
+    for event in events:
+        event_list.append(event.to_dict())
+    return jsonify(event_list)
+
+app.route("/events/<int:id>", methods = ["GET"])
+def get_oneEvent(event_id):
+    for event in events:
+        if event.id == event_id:
+            return (event.to_dict())
+    return "event not found",404
+
 @app.route("/events", methods=["POST"])
 def create_event():
-    # TODO: Task 2 - Design and Develop the Code
-    id = 0
-    for event in event:
-        id +=1
+   
     data = request.get_json()
-    new_event = Event(id+1, "Python Lab Review")
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-events.append(new_event)  
-  # TODO: Task 4 - Return and Handle Results
-  return jsonify(new_event.to_dict()),201
+    new_id = events[len(events)-1].id +1
 
-    pass
+    new_event = Event(new_id,data["title"])
+    events.append(new_event)
+
+    return jsonify(new_event.to_dict()),201
+
+
+
 
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
+    data = request.get_json()
 
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    for event in events:
+        if event.id == event_id:
+            event.title = data["title"]
+            return jsonify(event.to_dict())
+    return "Event not Found",404
 
-# TODO: Task 1 - Define the Problem
-# Remove an event from the list
+
+
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    for event in events:
+        if event.id == event_id:
+            events.remove(event)
+            return "",204
+    return "Event not Found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
